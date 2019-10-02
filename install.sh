e8170770e124a56035f624f84b82ea1aa7cde95b
Joshua Senouf <jsenouf@MTLws032.mtloutpost.com>
2019-10-02 16:17:52 -0400

    Added support for argument variables validity, aborting the process if one is empty for whatever the reason. Added helper environment variables to locate some key parts of a package easily. Misc fixes.

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 set -e
 
 INSTALL_PATH=${REZ_BUILD_INSTALL_PATH}
-PYSIDE2_URL=$1
-PYSIDE2_VERSION=${REZ_BUILD_PROJECT_VERSION}
+PYSIDE_URL=$1
+PYSIDE_VERSION=${REZ_BUILD_PROJECT_VERSION}
 
 # We print the arguments passed to the Bash script.
 echo -e "\n"
@@ -15,17 +15,27 @@ echo -e "==============="
 echo -e "\n"
 
 echo -e "[INSTALL][ARGS] INSTALL PATH: ${INSTALL_PATH}"
-echo -e "[INSTALL][ARGS] PYSIDE2 URL: ${PYSIDE2_URL}"
-echo -e "[INSTALL][ARGS] PYSIDE2 VERSION: ${PYSIDE2_VERSION}"
+echo -e "[INSTALL][ARGS] PYSIDE2 URL: ${PYSIDE_URL}"
+echo -e "[INSTALL][ARGS] PYSIDE2 VERSION: ${PYSIDE_VERSION}"
+
+# We check if the arguments variables we need are correctly set.
+# If not, we abort the process.
+if [[ -z ${INSTALL_PATH} || -z ${PYSIDE_URL} || -z ${PYSIDE_VERSION} ]]; then
+    echo -e "\n"
+    echo -e "[INSTALL][ARGS] One or more of the argument variables are empty. Aborting..."
+    echo -e "\n"
+
+    exit 1
+fi
 
 # We install PySide2.
 echo -e "\n"
-echo -e "[INSTALL] Installing PySide2-${PYSIDE2_VERSION}..."
+echo -e "[INSTALL] Installing PySide2-${PYSIDE_VERSION}..."
 echo -e "\n"
 
 # We copy the necessary files to the install directory.
-pip3 install ${PYSIDE2_URL} --target ${INSTALL_PATH} --upgrade --no-dependencies
+pip3 install ${PYSIDE_URL} --target ${INSTALL_PATH} --upgrade --no-dependencies
 
 echo -e "\n"
-echo -e "[INSTALL] Finished installing PySide2-${PYSIDE2_VERSION}!"
+echo -e "[INSTALL] Finished installing PySide2-${PYSIDE_VERSION}!"
 echo -e "\n"
